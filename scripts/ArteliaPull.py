2485df484f0871558fed87683ce18cb4b2938735
tourRe <alex.tourre@gmail.com>
2016-05-31 07:56:09 -0400

    added json to dict and table print

--- scripts/ArteliaPull.py
+++ scripts/ArteliaPull.py
@@ -1,25 +1,36 @@
 #!/usr/bin/env python
 
-import urllib2;
+import urllib2
+import json
 
 # cookies
-cookies = ('Cookie', '_ga=GA1.2.1862213469.1457357349');
+cookies = ('Cookie', '_ga=GA1.2.1862213469.1457357349')
 
 # url opener
-artelia = urllib2.build_opener();
-artelia.addheaders.append(cookies);
-address = "https://payg.angazadesign.com/api/";
+artelia = urllib2.build_opener()
+artelia.addheaders.append(cookies)
+address = "https://payg.angazadesign.com/api/"
 
 # interesting urls
 # "https://payg.angazadesign.com/api/accounts/2646511"
 # "https://payg.angazadesign.com/api/organizations/OR29"
 # "https://payg.angazadesign.com/api/payments?organization_qid=OR000028&offset=0&sort_by=recorded&limit=20&descending=true"
 
-org = str(input('Organization number: '));
+org = str(input('Organization number: '))
 
 # opener calls
-call_0 = artelia.open(address + "organizations/OR" + org);
-print call_0.read();
+call0 = artelia.open(address + "organizations/OR" + org)
+call1 = artelia.open(address + "payments?organization_qid=OR0000" + org +  "&offset=0&sort_by=recorded&limit=50&descending=true")
 
-call_1 = artelia.open(address + "payments?organization_qid=OR0000" + org +  "&offset=0&sort_by=recorded&limit=10&descending=true");
-print call_1.read();
+# converting json to dict
+str0 = call0.readline()
+data0 = json.loads(str0)
+
+str1 = call1.readline()
+data1 = json.loads(str1)
+
+# printing payment data
+payments = data1['_embedded']['item']
+for i in range(0,len(payments)-1):
+    data = payments[i]
+    print data['recorded']['value'] + " " + data['amount']['value'] + " " + data['currency']
