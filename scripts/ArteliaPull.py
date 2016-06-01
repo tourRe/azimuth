294995797bb30197b13c47dbf930cadac94fd9a6
tourRe <alex.tourre@gmail.com>
2016-06-01 06:52:45 -0400

    optimized json fetching and conversion

--- scripts/ArteliaPull.py
+++ scripts/ArteliaPull.py
@@ -2,36 +2,35 @@
 import urllib2
 import json
 
-# cookies
-cookies = ('Cookie', '_ga=GA1.2.1862213469.1457357349')
-
 # url opener
+cookies = ('Cookie', '_ga=GA1.2.1862213469.1457357349')
 artelia = urllib2.build_opener()
 artelia.addheaders.append(cookies)
+
+# api call parameters
 address = "https://payg.angazadesign.com/api/"
-
-# interesting urls
-# "https://payg.angazadesign.com/api/accounts/2646511"
-# "https://payg.angazadesign.com/api/organizations/OR29"
-# "https://payg.angazadesign.com/api/payments?organization_qid=OR000028&offset=0&sort_by=recorded&limit=20&descending=true"
-
 org = str(input('Organization number: '))
 num = str(input('Number of lines: '))
 
-# opener calls
-call0 = artelia.open(address + "organizations/OR" + org)
-call1 = artelia.open(address + "payments?organization_qid=OR0000" + org +  "&offset=0&sort_by=recorded&limit=" + num + "&descending=true")
+# Fetches the Json file at a given url and converts it to a dictionary
+def to_dict(url):
+    call = artelia.open(url)
+    str_json = call.readline()
+    return json.loads(str_json)
 
-# converting json to dict
-str0 = call0.readline()
-data0 = json.loads(str0)
+# 1.Organization
+org_dict = to_dict(address + "organizations/OR" + org)
+print org_dict['name']
 
-str1 = call1.readline()
-data1 = json.loads(str1)
-
-# printing payment data
-print data0['name']
-payments = data1['_embedded']['item']
+# 2.Payments
+pay_dict = to_dict(address + "payments?organization_qid=OR0000" + org +  "&offset=0&sort_by=recorded&limit=" + num + "&descending=true")
+payments = pay_dict['_embedded']['item']
 for i in range(0,len(payments)-1):
     data = payments[i]
     print data['recorded']['value'] + " " + data['amount']['value'] + " " + data['currency']
+
+# 3.Accounts
+
+# 4.Agents
+
+
