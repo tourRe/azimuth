eddd3a6e1f64eefd2df65e58a2a4cf2ef02a2dd2
tourRe <alex.tourre@gmail.com>
2016-06-01 23:26:31 -0400

    added a save feature to external file

--- scripts/ArteliaPull.py
+++ scripts/ArteliaPull.py
@@ -26,11 +26,15 @@ org_name = org_dict['name']
 org_products = org_dict['available_product_types']
 org_smsPrice = org_dict['sms_price']['value']
 
+# Preparing dump dict and keys to read
 dump={}
 ko_keys=['za:intake_report','za:replacements_report','za:units','za:sms_messages_report','za:refunds', 'za:users','za:units_sold_by_type_data','za:revenue_data','type','za:call_sequences','za:credit_adjustments_received','za:logo','za:accounts_data','za:agents_by_units_sold_data','za:accounts_by_state_data']
-ok_keys = [u'za:activations', u'za:portals', u'za:sms_messages', u'za:ledger_register', u'za:summary_data', u'za:accounts', u'self', u'za:receipts', u'za:groups', u'za:registration_config', u'za:remittances', u'za:post_user', u'za:ledger_balance', u'za:metrics_data', u'za:post_accounts', u'za:payments', u'za:replacements', u'za:exchange_data']
+ok_keys = [u'za:activations', u'za:portals', u'za:sms_messages', u'za:ledger_register', u'za:accounts', u'za:summary_data', u'self', u'za:receipts', u'za:groups', u'za:registration_config', u'za:remittances', u'za:post_user', u'za:ledger_balance', u'za:metrics_data', u'za:post_accounts', u'za:payments', u'za:replacements', u'za:exchange_data']
 skip_keys = []
 
+# Fecthing data
+# Need to change code so that all values are stored in the dump dictionar
+# (nested)
 for key in org_dict['_links']:
     if ((key in ko_keys) or ('report' in key)):
         print 'invalid ' + key
@@ -40,17 +44,16 @@ for key in org_dict['_links']:
         print 'skipping ' + key
     else:
         print 'fetching ' + key
-        show = input('execute? Y/N ')
+        # show = input('execute? Y/N ')
+        show = 'Y'
         if show == 'Y':
             print org_dict['_links'][key]['href']
             print org_dict['_links'][key]['href'].split('{')[0]
-            dump['key']=to_dict(org_dict['_links'][key]['href'].split('{')[0])
-            print dump['key']
+            dump[key]=to_dict(org_dict['_links'][key]['href'].split('{')[0])
 
-for key in dump:
-    print 'high level key ' + key
-    for key2 in dump['key']:
-        print key2
+# Saving data to JSON
+with open('/Users/alexandre/azimuth/scripts/dump.json', 'w') as f:
+    json.dump(dump, f)
 
 # 2.Payments
 
