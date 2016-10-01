98047e45972207f2422a1e39f1eb0a1f795e85e2
tourRe <alex.tourre@gmail.com>
2016-10-01 18:46:22 +0000

    prepared views, urls and templates for sales app

--- pulse/sales/views.py
+++ pulse/sales/views.py
@@ -10,6 +10,15 @@ def index(request):
             }
     return render(request, 'sales/index.html', context)
 
+def manager_index(request):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/manager_index.html', context)
+
 def manager(request, manager_firstname):
     agent_list = Agent.objects.order_by('location')
     manager_list = Manager.objects.order_by('firstname')
@@ -19,6 +28,15 @@ def manager(request, manager_firstname):
             }
     return render(request, 'sales/manager.html', context)
 
+def agent_index(request):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/agent_index.html', context)
+
 def agent(request, agent_login):
     agent_list = Agent.objects.order_by('location')
     manager_list = Manager.objects.order_by('firstname')
@@ -27,3 +45,48 @@ def agent(request, agent_login):
             'agent_list' : agent_list
             }
     return render(request, 'sales/agent.html', context)
+
+def client_index(request):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/client_index.html', context)
+
+def client(request, client_pk):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/client.html', context)
+
+def account_index(request):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/account_index.html', context)
+
+def account(request, account_Angaza):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/account.html', context)
+
+def payment_index(request):
+    agent_list = Agent.objects.order_by('location')
+    manager_list = Manager.objects.order_by('firstname')
+    context = {
+            'manager_list' : manager_list,
+            'agent_list' : agent_list
+            }
+    return render(request, 'sales/payment_index.html', context)
