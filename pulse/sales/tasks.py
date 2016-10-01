60af8dfcb3c7a9dc9e8b47351a5e4aeabcb0f16c
tourRe <alex.tourre@gmail.com>
2016-10-01 10:15:50 +0000

    tasked account update (no delete)

--- pulse/sales/tasks.py
+++ pulse/sales/tasks.py
@@ -24,28 +24,52 @@ def collect():
         # Creating or identifying client
         try: gender = acc_read['customer_gender'][0]
         except: gender = acc_read['customer_gender']
-        client, created = Client.objects.get_or_create(
-                name = acc_read['owner_name'],
-                gender = gender,
-                phone = acc_read['owner_msisdn'],
-                location = acc_read['location']
-                )
+        # if the client exists (based on phone), updates info
+        try:
+            client = Client.objects.get(phone = acc_read['owner_msisdn'])
+            client.name = acc_read['owner_name']
+            client.gender = gender
+            client.location = acc_read['location']
+        # if not creates a new client
+        except:
+            client = Client(
+                    name = acc_read['owner_name'],
+                    gender = gender,
+                    phone = acc_read['owner_msisdn'],
+                    location = acc_read['location']
+                    )
         client.save()
 
         # Creating or identifying account
-        acc, created = Account.objects.get_or_create(
+        # if the account exists (based on acc #), updates info
+        try: 
+            acc = Account.objects.get(
                 account_Angaza = acc_read['angaza_id'],
-                account_GLP = acc_read['account_number'],
-                client = client,
-                plan_name = acc_read['group_name'],
-                plan_product = product,
-                plan_up = int(acc_read['upfront_price']),
-                plan_tot = int(acc_read['unlock_price']),
-                plan_week = int(float(acc_read['hour_price'])*24*7),
-                reg_date = toDate_ms(acc_read['registration_date_utc']),
-                agent = agent,
-                status = acc_read['account_status'][0].lower()
-                )
+                account_GLP = acc_read['account_number'])
+            acc.client = client
+            acc.plan_name = acc_read['group_name']
+            acc.plan_product = product
+            acc.plan_up = int(acc_read['upfront_price'])
+            acc.plan_tot = int(acc_read['unlock_price'])
+            acc.plan_week = int(float(acc_read['hour_price'])*24*7)
+            acc.reg_date = toDate_ms(acc_read['registration_date_utc'])
+            acc.agent = agent
+            acc.status = acc_read['account_status'][0].lower()
+        # otherwise creates a new account
+        except:
+            acc = Account(
+                    account_Angaza = acc_read['angaza_id'],
+                    account_GLP = acc_read['account_number'],
+                    client = client,
+                    plan_name = acc_read['group_name'],
+                    plan_product = product,
+                    plan_up = int(acc_read['upfront_price']),
+                    plan_tot = int(acc_read['unlock_price']),
+                    plan_week = int(float(acc_read['hour_price'])*24*7),
+                    reg_date = toDate_ms(acc_read['registration_date_utc']),
+                    agent = agent,
+                    status = acc_read['account_status'][0].lower()
+                    )
         acc.save()
 
 def csvToList(path):
