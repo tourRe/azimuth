bdacde41dbf380460772b306f2fcc68c3ae57794
tourRe <alex.tourre@gmail.com>
2016-12-04 13:47:47 +0000

    improved updates displayw

--- pulse/pulse/views.py
+++ pulse/pulse/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import Http404
+from system.models import Update
+
+updates = Update.objects.all()
+last_update = updates.last_update()
+last_full_update = updates.last_full_update()
+
+context = {}
+context['last_update'] = last_update
+context['last_full_update'] = last_full_update
 
 def index(request):
-    context = {}
     return render(request, 'index.html', context)
